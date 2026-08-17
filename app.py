f22f578e60823f1fa237662fce0454835c1e4d58
s-kulal <thefirefist98@gmail.com>
2026-08-17 22:43:06 +0530

    Token bearer add

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ users = {               # fake database, it is just python dictionary
     "subhash": "user123"
 }
 
+tokens = {}
+
 @app.route("/")     # Home endpoint -> decorator
 def home(): # this runs when someone reaches "/"
     return {
@@ -25,6 +27,8 @@ def login(): # login function
     if username in users and users[username] == password: #authentication check
         token = username + '-token'
 
+        tokens[token] = username  # stored inside the tokens = {}
+
         return jsonify({   # success response
             "token": token
         }), 200
@@ -32,6 +36,27 @@ def login(): # login function
     return jsonify({
         "error": "Invalid Credentials"
     }), 401
+
+@app.route("/profile")
+def profile():
+
+    auth_header = request.headers.get("Authorization") # gets "Bearer admin-token"
+
+    if not auth_header:
+        return jsonify({
+            "error": "Token Missing"
+        }), 401
+
+    token = auth_header.replace("Bearer ", "") # extract token  -> admin-token
+
+    if token not in tokens: # validate tokens
+        return jsonify({
+            "error": "Invalid Token"
+        }), 401
+
+    return jsonify({
+        "username": tokens[token]
+    }), 200
     
 
 if __name__ == "__main__": # start server -> run this only if app.py is executed
