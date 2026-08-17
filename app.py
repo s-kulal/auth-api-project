437e496e750a70bd9deeea9b6831e16ec77e2a08
s-kulal <thefirefist98@gmail.com>
2026-08-17 22:18:07 +0530

    Add Login Cred

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask 
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+users = {
+    "admin": "admin123",
+    "subhash": "user123"
+}
 
 @app.route("/")
 def home():
     return {"message":"Auth API Project"}
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    username = data.get("username")
+    password = data.get("password")
+
+    if username in users and users[username] == password:
+        token = username + '-token'
+
+        return jsonify({
+            "token": token
+        }), 200
+
+    return jsonify({
+        "error": "Invalid Credentials"
+    }), 401
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
