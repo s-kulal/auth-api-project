63499b47dcd3286e4df1d5d6b4f0391bd50a7f0e
s-kulal <thefirefist98@gmail.com>
2026-08-17 22:30:48 +0530

    Comments add

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__) # create Flask application 
 
-users = {
+users = {               # fake database, it is just python dictionary 
     "admin": "admin123",
     "subhash": "user123"
 }
 
-@app.route("/")
-def home():
-    return {"message":"Auth API Project"}
+@app.route("/")     # Home endpoint -> decorator
+def home(): # this runs when someone reaches "/"
+    return {
+        "message":"Auth API Project"
+    } # client receives this message
 
-@app.route("/login", methods=["POST"])
-def login():
-    data = request.get_json()
+@app.route("/login", methods=["POST"])    # login endpoint
+# POST is used bcs we are sending users ={"admin" ...} to the server
+def login(): # login function 
+    data = request.get_json()  # read json 
+    # now dats contains user ={"username": "admin", ...}
 
     username = data.get("username")
     password = data.get("password")
 
-    if username in users and users[username] == password:
+    if username in users and users[username] == password: #authentication check
         token = username + '-token'
 
-        return jsonify({
+        return jsonify({   # success response
             "token": token
         }), 200
 
@@ -30,8 +34,8 @@ def login():
     }), 401
     
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__": # start server -> run this only if app.py is executed
+    app.run(debug=True) # launch flask 
 
 
 
