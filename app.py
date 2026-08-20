8d9049f0ab319ae5e75ba9a799ef8cee5bafab95
s-kulal <thefirefist98@gmail.com>
2026-08-20 16:01:05 +0530

    Implement role based access control

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ def home(): # this runs when someone reaches "/"
         "message":"Auth API Project"
     } # client receives this message
 
+# Login endpoint
+
 @app.route("/login", methods=["POST"])    # login endpoint
 # POST is used bcs we are sending users ={"admin" ...} to the server
 def login(): # login function 
@@ -47,20 +49,28 @@ def login(): # login function
         "error": "Invalid Credentials"
     }), 401
 
+
+# Authenticate user    ->    Function definition 
+
+def authenticate():
+    auth_header = request.headers.get("Authorization") # gets "Bearer admin-token"
+    
+    if not auth_header:
+        return None
+    
+    token = auth_header.replace("Bearer ", "") # replace "Bearer " with "" then 
+                                               # extract token  -> admin-token
+    return tokens.get(token)                                   
+    # bcs the endpoint needs {"username": "admin", "role":"admin"}
+
+# Profile endpoint
+
 @app.route("/profile")
 def profile():
 
-    auth_header = request.headers.get("Authorization") # gets "Bearer admin-token"
+    user = authenticate()
 
-    if not auth_header:
-        return jsonify({
-            "error": "Token Missing"
-        }), 401
-
-    token = auth_header.replace("Bearer ", "") # replace "Bearer " with "" then 
-                                               # extract token  -> admin-token
-
-    if token not in tokens: # validate tokens
+    if not user: # validate tokens
         return jsonify({
             "error": "Invalid Token"
         }), 401
@@ -69,35 +79,41 @@ def profile():
         "username": tokens[token]
     }), 200
 
+
+# Create user endpoint
+
 @app.route("/create-user", methods=["POST"])
 def create_user():
 
-    auth_header = request.headers.get("Authorization")  # exact header 
-    # receives Authorization: Bearer admin-token 
-    if not auth_header:
-        return jsonify({
-            "error": "Token Missing"
-        }), 401
+    user = authenticate()
 
-    token = auth_header.replace("Bearer ", "")   # token = admin-token 
-                                                 # bcs bearer is replaced with ""
-    if token not in tokens:         # Check token
+    if not user:         # Check token  -> from the authentication
         return jsonify({
             "error": "Invalid Token"
         }), 401
-
-    user = tokens[token]       # Retrieves Users data -> admin, subhash, etc
 
     if user["role"] != "admin": # aunthenticating the role 
         return jsonify({        # to decide the authorization
             "error": "Forbidden"# highest previlage is given to admin only 
         }), 403                 # or depends but usully admin 
 
-    return jsonify({
-        "message": "User Created Successfully"  # Status code 201 -> user CREATED   
-    }), 201
-    
+    # to create new user
+    data = request.get_json() # once they enter details fetched to data
 
+    username = data.get("username")
+    password = data.get("password")
+    role = data.get("role")
+
+    users[username] = {
+        "password" : password,
+        "role" : role
+    }
+
+    return jsonify({
+        "message": "User Created Successfully"
+    }), 201
+
+# Launch FLASK
 if __name__ == "__main__": # start server -> run this only if app.py is executed
     app.run(debug=True) # launch flask 
 
